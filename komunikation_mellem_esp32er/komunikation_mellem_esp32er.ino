2d93e50c76f05678cf7880846fb57518edc367b5
ChristofferJelle <christofferjt@gmail.com>
2025-11-09 16:05:37 +0100

    Create komunikation_mellem_esp32er.ino

---
+++ komunikation_mellem_esp32er/komunikation_mellem_esp32er.ino
@@ -0,0 +1,63 @@
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.  
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+#include <WiFi.h>
+#include <esp_wifi.h>
+
+struct Sensordata {
+    float temp;
+    float hum;
+    float pres;
+}; 
+  // Create a struct to hold sensor readings
+Sensordata outgoingStruct;
+
+/*
+esp_wifi_get_mac: attempts to Get mac of specified interface (ifx) in its own acces point
+and store mac of the interface ifx in an array.
+then returns if it succesded or not in getting the mac
+
+then with printf i specify it the format it needs to spit out the mac address in. %02x = print as hexidecimal number, that needs to be at least 2 digits wide.
+*/
+uint8_t* readMacAddress() {
+  static uint8_t baseMac[6];
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_AP, baseMac);
+  if (ret == ESP_OK) {
+    return baseMac;
+  } else {
+    Serial.println("Failed to read MAC address");
+    return nullptr;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+
+
+
+//initialises what kinda wifi mode its in, rn its in accese point mode, so i dont have to connect to uni wifi with own student password.
+  WiFi.mode(WIFI_AP);
+  WiFi.AP.begin();
+
+  Serial.print("[DEFAULT] ESP32 Board MAC Address: ");
+  uint8_t* ownMac = readMacAddress();
+
+  //converts array into a string.
+  if (ownMac != nullptr) {
+    String ownMacHex;
+    for (int i = 0; i < 6; i++) {
+      String hexPart = String(ownMac[i], HEX);
+      if (hexPart.length() < 2) hexPart = "0" + hexPart;  // zero-pad if needed
+      ownMacHex += hexPart;
+      if (i < 5) ownMacHex += ":";  // add ':' except after last byte
+    }
+    Serial.print(ownMacHex);
+  }
+}
+
+void loop() {
+}
