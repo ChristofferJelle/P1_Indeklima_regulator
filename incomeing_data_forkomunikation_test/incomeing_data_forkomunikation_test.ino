f9d964ff8b413151799860c7b8ed281430b1757d
ChristofferJelle <christofferjt@gmail.com>
2025-11-10 12:28:55 +0100

    testing data transfer

--- incomeing_data_forkomunikation_test/incomeing_data_forkomunikation_test.ino
+++ incomeing_data_forkomunikation_test/incomeing_data_forkomunikation_test.ino
@@ -1,30 +1,19 @@
 #include <esp_now.h>
 #include <WiFi.h>
 #include <esp_wifi.h>
-#include <string.h>
+
 struct Sensordata {
   float temp;
   float hum;
   float pres;
 };
 // Create a struct to hold sensor readings
-Sensordata outgoingStruct;
+Sensordata IngoingStruct;
 
-// Variable to store if sending data was successful
-String success;
-
-//adress of the mainESP32 in sending data to
-uint8_t broadcastAddress[] = { 0x08, 0x3a, 0xf2, 0x45, 0x3f, 0x51 };
-
-
-
-
-/*
-esp_wifi_get_mac: attempts to Get mac of specified interface (ifx) in its own acces point
-and store mac of the interface ifx in an array.
-then returns if it succesded or not in getting the mac
-
-then with printf i specify it the format it needs to spit out the mac address in. %02x = print as hexidecimal number, that needs to be at least 2 digits wide.
+// Define variables to store incoming readings
+float incomingTemp;
+float incomingHum;
+float incomingPres;
 
 
 uint8_t* readMacAddress() {
@@ -37,23 +26,20 @@ uint8_t* readMacAddress() {
     return nullptr;
   }
 }
-*/
 
-//parameter automatically get fillede when callback funktion is called.
-void OnDataSent(const uint8_t* mac_addr, esp_now_send_status_t status) {
-  Serial.print("Send to: ");
-  for (int i = 0; i < 6; i++) {
-    Serial.printf("%02X", mac_addr[i]);
-    if (i < 5) Serial.print(":");
-  }
-  Serial.print(" -> ");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Failed");
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&IngoingStruct, incomingData, sizeof(IngoingStruct));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  incomingTemp = IngoingStruct.temp;
+  incomingHum = IngoingStruct.hum;
+  incomingPres = IngoingStruct.pres;
 }
 
 void setup() {
   Serial.begin(115200);
 
-/*
   //initialises what kinda wifi mode its in, rn its in accese point mode, so i dont have to connect to uni wifi with own student password.
   WiFi.mode(WIFI_AP);
   WiFi.AP.begin();
@@ -71,30 +57,19 @@ void setup() {
     }
     Serial.print(ownMacHex);
   }
-    */
 
   //initatite ESP_now for comunication without router.
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
+
   //Register that i want to send to cb. basically says "everytime i send data u just need to run this too"
-  esp_now_register_send_cb(esp_now_send_cb_t(OnDataSent));
+  esp_now_register_send_cb(esp_now_send_cb_t(OnDataRecv));
 
-  // Register peer
-  esp_now_peer_info_t peerInfo;
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;
-  peerInfo.encrypt = false;
-
-  // Add peer
-  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("Failed to add peer");
-    return;
-  }
+  // Register peers
 
 }
-
 
 void loop() {
 }
