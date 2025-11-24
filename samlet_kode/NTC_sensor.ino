db33afd38f54cde0aff11100a94f353212aa2313
LuccaDKjaergaard <lucc0108.skole@gmail.com>
2025-11-24 11:55:04 +0100

    slave code samlet

---
+++ samlet_kode/NTC_sensor.ino
@@ -0,0 +1,23 @@
+float NTCRead(bool tempstate) {
+  float rConst = 100000.0; //resistor in voltage divider
+  float vSource = 5; //output voltage from Arduino
+  float vOut = (analogRead(ntcPin) / 1023.0) * vSource; //output voltage from middle of voltage divider
+  float ntc = (vOut * rConst) / (vSource - vOut); //calculate resistance of NTC
+
+  //Steinhart-Hart coefficients from datasheet
+  //for NTCLE100E3104JBO
+  float A = 3.354016e-03;
+  float B = 2.460382e-04;
+  float C = 3.405377e-06;
+  float D = 1.034240e-07;
+  float R25 = 100000; //reference resistance
+
+  float tempK = 1.0/( A + B*log(ntc/R25) + C*(pow(log(ntc/R25), 2.0)) + D*(pow(log(ntc/R25), 3.0)) ); //calculate temperature in K
+  float tempC = tempK - 273.15; //calculate temperature in C
+
+  if(tempstate) {
+    return tempC;
+  } else if(!tempstate) {
+    return tempK;
+  }
+}
---
+++ samlet_kode/humidity_reader.ino
@@ -0,0 +1,11 @@
+float dhtRead() {
+  //reads can take up to 250ms
+  float humidity = dht.readHumidity();
+
+  if (isnan(humidity)) { //check if any reads failed and exit early
+    Serial.println("Failed to read humidity.");
+    return;
+  } else {
+    return humidity;
+  }
+}
