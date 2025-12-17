29eef7996ba80666e6b1f8caa5c8975fa8a88753
ChristofferJelle <christofferjt@gmail.com>
2025-12-17 13:44:00 +0100

    Update Slave_Sending_funktions.ino

--- samlet_kode/Slave_Sending_funktions.ino
+++ samlet_kode/Slave_Sending_funktions.ino
@@ -37,7 +37,7 @@ void OnDataRecv(const uint8_t* mac, const uint8_t* incomingData, int len) {
     case 'S':  //send to master
       outgoingStruct.hum = dhtRead();
       outgoingStruct.temp = NTCRead(true);
-      outgoingStruct.co2 = readPPM(110.47, -2.862, 98.8334613);
+      outgoingStruct.co2 = readPPM(110.47, -2.862, 99.6555108);
       SendDataToMaster();
       break;
     case 'R':
