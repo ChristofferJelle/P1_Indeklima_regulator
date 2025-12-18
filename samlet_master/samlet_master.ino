dd821ca0dd6aafcc15f4ad69333579300b673636
ChristofferJelle <christofferjt@gmail.com>
2025-12-18 17:57:26 +0100

    Update samlet_master.ino

--- samlet_master/samlet_master.ino
+++ samlet_master/samlet_master.ino
@@ -33,7 +33,7 @@ TFT_eSPI tft = TFT_eSPI();  // Create TFT object
 #define CLK_PIN 37  //1st click
 #define DT_PIN 38   //2nd click
 #define SW_PIN 39   //button click
-bool interrupt = false;
+
 //lower limit values need to be temp 15 and humid 30%
 struct SensorDataLimitTp {
   long Temp = 25;
