c2b5a545334041df83b58ae59333ea3a36d421ed
christoffer <christofferjt@gmail.com>
2025-12-18 10:11:02 +0100

    Update servo_motor.ino

--- samlet_master/servo_motor.ino
+++ samlet_master/servo_motor.ino
@@ -1,17 +1,21 @@
 void ServoClose(unsigned long startTime) {
-  moveServo(startTime, 180, 0); //go from 180 to 0 degrees
-  servoState = sweepClose;
+  if (servo.read() > 0) {
+    moveServo(startTime, 180, 0);  //go from 180 to 0 degrees
+    servoState = sweepClose;
+  }
 }
 
 void ServoOpen(unsigned long startTime) {
-  moveServo(startTime, 0, 180); //go 0 to 180 degrees
-  servoState = sweepOpen;
+  if (servo.read() < 178) {
+    moveServo(startTime, 0, 180);  //go 0 to 180 degrees
+    servoState = sweepOpen;
+  }
 }
 
 void moveServo(unsigned long startTime, int startAngle, int stopAngle) {
   unsigned long progress = millis() - startTime;
 
-  if(progress <= servoActionTime) {
+  if (progress <= servoActionTime) {
     long angle = map(progress, 0, servoActionTime, startAngle, stopAngle);
     servo.write(angle);
   }
@@ -24,11 +28,10 @@ float ShuntCurrent() {
   float avgVolt = 0;
   int samples = 350;
   for (int i = 0; i < samples; i++) {
-    avgVolt += 3.33  * analogRead(SHUNT_PIN) / (pow(2.0, 12.0) - 1.0);
+    avgVolt += 3.33 * analogRead(SHUNT_PIN) / (pow(2.0, 12.0) - 1.0);
   }
   avgVolt = avgVolt / samples;
 
   float current = avgVolt / modstand;
   return current;
 }
-
